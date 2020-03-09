9525742ac44ab60b16df7ac3281dda8c0c5819fb
Federico Fuga <fuga@studiofuga.com>
2020-03-09 11:30:12 +0100

    Create Community Confirm command added
    Signed-off-by: Federico Fuga <fuga@studiofuga.com>

--- app/src/main/java/org/endcoronavirus/outreach/CreateCommunityFragment.java
+++ app/src/main/java/org/endcoronavirus/outreach/CreateCommunityFragment.java
@@ -2,6 +2,9 @@ package org.endcoronavirus.outreach;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,20 +19,31 @@ public class CreateCommunityFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_community_create, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
 
-        /*
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(CreateCommunityFragment.this)
-                        .navigate(R.id.add_community_action);
-            }
-        });*/
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_community_create, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.command_menu_confirm) {
+            NavHostFragment.findNavController(CreateCommunityFragment.this)
+                    .navigate(R.id.action_confirm_community_create);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
